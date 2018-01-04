34d00a447648463f08949805d35d159eb87aee66
jb110191 <jb110191@hotmail.fr>
2018-01-04 12:37:42 +0100

    Ajout de commentaire dans le main.c et modification de l'affichage

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Programme       :   Hello World
+Programmation   :   jb110191
+*/
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("Bonjour,\n");
+    printf("je suis un programme ecrit en C et qui affiche du texte dans une console!\n");
+
     return 0;
 }
